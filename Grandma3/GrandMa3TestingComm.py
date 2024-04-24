b93ae877c88df7dd31cc91fa5f341c884b6f18ba
Alastair <Yongen.teo@gmail.com>
2024-04-24 13:30:52 +0800

    Update GrandMa3TestingComm.py

--- Grandma3/GrandMa3TestingComm.py
+++ Grandma3/GrandMa3TestingComm.py
@@ -9,15 +9,16 @@ def send_message(receiver_ip, receiver_port, address, message):
 		# Send an OSC message to the receiver
 		client.send_message(address, message)   
 
-		print("Message sent successfull	y.")
+		print("Message sent successfully.")
 	except:
-		print("Message not sent")
+		print("Unsuccesful")
 
 #change the below values
 if __name__ == "__main__":
-    LAPTOP_IP = "192.168.1.61"		# send to laptop w grandMA3
+    LAPTOP_IP = "192.168.254.142"		# send to laptop w grandMA3
     PORT = 8000                     # laptop w grandMA3 port number
-    addr = "/gma3/Page1/Fader201"
+    addr = "/gma3/cmd/"				# Keep it as it is
 
-    send_message(LAPTOP_IP, PORT, addr, "Go to Sequence 1")
+
+    send_message(LAPTOP_IP, PORT, addr, "Go+ Exec 201")  #Commands to control the Playback and other Gma3 controls
 	
