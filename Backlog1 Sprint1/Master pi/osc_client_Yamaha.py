9205b0c235ed80e968bb11a2eb06dc3f6db57157
Isaac Micla <106132526+isaacgsm@users.noreply.github.com>
2024-04-26 12:40:46 +0800

    Convert gui values to yamaha values

--- Backlog1 Sprint1/Master pi/osc_client_Yamaha.py
+++ Backlog1 Sprint1/Master pi/osc_client_Yamaha.py
@@ -1,5 +1,6 @@
 # Huats 2023 oscstarterkit
 from pythonosc import udp_client
+import gui
 
 def send_message(receiver_ip, receiver_port, address, message):
 	try:
@@ -27,52 +28,92 @@ addr = "/print"
 	msg = "Client pi is online"
 	send_message(PI_A_ADDR, PORT, addr, msg)'''
 
+# convert GUI values to yamaha values
+def rangeconvert1():
+	OldValue = gui.var1
+	OldMax = 100
+	OldMin = 0
+	NewMax = 1000
+	NewMin = -32768
+	OldRange = (OldMax - OldMin)  
+	NewRange = (NewMax - NewMin)  
+	global NewValue1
+	NewValue1 = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin
+
+def rangeconvert2():
+	OldValue = gui.var2
+	OldMax = 100
+	OldMin = 0
+	NewMax = 1000
+	NewMin = -32768
+	OldRange = (OldMax - OldMin)  
+	NewRange = (NewMax - NewMin)  
+	global NewValue2
+	NewValue2 = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin
+
+def rangeconvert3():
+	OldValue = gui.var3
+	OldMax = 100
+	OldMin = 0
+	NewMax = 1000
+	NewMin = -32768
+	OldRange = (OldMax - OldMin)  
+	NewRange = (NewMax - NewMin)  
+	global NewValue3
+	NewValue3 = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin
+
 def yamahafader1Up():
+	rangeconvert1()
 	global PI_A_ADDR
 	global PORT
 	global addr
-	#msg = "OSC Sequence 1 from pi"
-	msg = "set MIXER:Current/InCh/Fader/Level 0 0 1000 "	
+	msg = "set MIXER:Current/InCh/Fader/Level 0 0 {NewValue1} "	
+	print(NewValue1)
 	send_message(PI_A_ADDR, PORT, addr, msg)
 
 def yamahafader1Down():
+	rangeconvert1()
 	global PI_A_ADDR
 	global PORT
 	global addr
-	#msg = "OSC Sequence 1 from pi"
-	msg = "set MIXER:Current/InCh/Fader/Level 0 0 -32768 "	
+	msg = "set MIXER:Current/InCh/Fader/Level 0 0 {NewValue1} "	
+	print(NewValue1)
 	send_message(PI_A_ADDR, PORT, addr, msg)
 	
 def yamahafader2Up():
+	rangeconvert2()
 	global PI_A_ADDR
 	global PORT
 	global addr
-	#msg = "OSC Sequence 2 from pi"
-	msg = "set MIXER:Current/InCh/Fader/Level 1 0 1000 "
+	msg = "set MIXER:Current/InCh/Fader/Level 1 0 {NewValue2} "
+	print(NewValue2)
 	send_message(PI_A_ADDR, PORT, addr, msg)
 	
 def yamahafader2Down():
+	rangeconvert2()
 	global PI_A_ADDR
 	global PORT
 	global addr
-	#msg = "OSC Sequence 2 from pi"
-	msg = "set MIXER:Current/InCh/Fader/Level 1 0 -32768 "
+	msg = "set MIXER:Current/InCh/Fader/Level 1 0 {NewValue2} "
+	print(NewValue2)
 	send_message(PI_A_ADDR, PORT, addr, msg)
 
 def yamahafader3Up():
+	rangeconvert3()
 	global PI_A_ADDR
 	global PORT
 	global addr
-	#msg = "OSC Sequence 2 from pi"
-	msg = "set MIXER:Current/InCh/Fader/Level 2 0 1000 "	
+	msg = "set MIXER:Current/InCh/Fader/Level 2 0 {NewValue3} "	
+	print(NewValue3)
 	send_message(PI_A_ADDR, PORT, addr, msg)
 
 def yamahafader3Down():
+	rangeconvert3()
 	global PI_A_ADDR
 	global PORT
 	global addr
-	#msg = "OSC Sequence 2 from pi"
-	msg = "set MIXER:Current/InCh/Fader/Level 2 0 -32768 "	
+	msg = "set MIXER:Current/InCh/Fader/Level 2 0 {NewValue3} "	
+	print(NewValue3)
 	send_message(PI_A_ADDR, PORT, addr, msg)
 
 #startupmsg()
