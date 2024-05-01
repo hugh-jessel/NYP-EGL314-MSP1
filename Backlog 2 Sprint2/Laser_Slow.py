fdd6eddefffef36451c21bbd82910483459f1d32
kevis <siangkevis@gmail.com>
2024-05-02 00:06:08 +0800

    Laser_fast & Laser_slow file
    incomplete , will be finished by thurs ...

---
+++ Backlog 2 Sprint2/Laser_Slow.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+
+def laser_on():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(21, GPIO.OUT)
+    
+    GPIO.output(21, GPIO.LOW)
+
+def laser_off():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(21, GPIO.OUT)
+    
+    GPIO.output(21, GPIO.HIGH)  # Turn off the relay
+    print('Relay OFF - Cleaning up GPIO.')
+    GPIO.cleanup()  
+
+def onBeat():
