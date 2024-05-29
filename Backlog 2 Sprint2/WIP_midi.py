3835b6b06dcad7f8cde11c417c197a5e0aaa4545
Anestesiaa-0512 <jfengtjf@gmail.com>
2024-05-29 18:15:10 +0800

    29/05 midi.py update

--- Backlog 2 Sprint2/WIP_midi.py
+++ Backlog 2 Sprint2/WIP_midi.py
@@ -1,108 +1,116 @@
 import mido
-import markers
+import Repear_markers
 import play_stop
+import time
 
-'''
+start_time = time.time()
+
+def game_over():
+    print ('Too slow')
+    Repear_markers.startMk()                                #Send back to the start
+    play_stop.play_stop()                              #To pause / not let the game immediately repeat
+    exit()                                  #Stop midi.py
+    
+def deflect_success():
+    
+    current_time = time.time()
+    elapsed_time = current_time - start_time
+    print(elapsed_time)
+    
+    #Send to deflect marker
+    count_aftDef = 0
+    count = 0
+    count_aftDef += 0.5
+    if count_aftDef == 0.5:
+        count == 0
+
 def Midi_LaunchPad_MK3():
     LaunchpadPro_Name = "Launchpad Pro MK3:Launchpad Pro MK3 LPProMK3 MIDI 28:0"
-
+    
     if LaunchpadPro_Name not in mido.get_input_names():
         print(f"Device {LaunchpadPro_Name} not found. Please check the device name.")
         return
 
-    
     with mido.open_input(LaunchpadPro_Name) as inport:
         print(f"Listening to {LaunchpadPro_Name} for note messages...")
+        count = 0
         try:
-            for msg in inport:
-                
-                if msg.type == 'note_on':
-                    # Note on messages represent pad presses
-                    print(f'Note On: Note={msg.note}')
-                    if msg.note == 60:#Red;60;North 
-                        print ('North')
-                        markers.marker_1()
-                        #play_stop.play_stop()
-                    elif msg.note == 65:#Green;65;South
-                        print ('South')
-                        markers.marker_2()
-                        #play_stop.play_stop()
-                    elif msg.note == 62:#Blue;62;East
-                        print ("East")
-                        markers.marker_3()
-                        #play_stop.play_stop()
-                    elif msg.note == 64:#Yellow;64;West
-                        print ("West")
-                        markers.marker_4()
-                        #play_stop.play_stop()
-                    #print(f'Note On: Note={msg.note} Velocity={msg.velocity}')
-                elif msg.type == 'note_off':
-                    # Note off messages represent pad releases
-                    print(f'Note Off: Note={msg.note} Page={}')
-                    #print(f'Note Off: Note={msg.note} Velocity={msg.velocity}')
-        except KeyboardInterrupt:
-                print("Stopped listening to MIDI messages.")
-
-if __name__ == "__main__":
-    Midi_LaunchPad_MK3()
-'''
-# Define custom mode program number (e.g., program 10 for custom mode)
-custom_mode_program = 7
-custom_mode_channel = 7
-
-CUSTOM_MODE_MESSAGES = [
-    [0xF0, 0x00, 0x20, 0x29, 0x02, 0x0D, 0x03, 0x00, 0xF7],  # Custom Mode 0
-    [0xF0, 0x00, 0x20, 0x29, 0x02, 0x0D, 0x03, 0x01, 0xF7],  # Custom Mode 1
-    # Add messages for Custom Modes 2-7 here
-]
-def Midi_LaunchPad_MK3():
-    LaunchpadPro_Name = "Launchpad Pro MK3:Launchpad Pro MK3 LPProMK3 MIDI 28:0"
-
-    outport = mido.open_output(LaunchpadPro_Name)
+            while True: 
+                for msg in inport:
+                    count += 1
+                    print(count)
+                    time.sleep(0.5)
+                    if msg.type == 'note_on':                                  # Note on messages represent pad presses
+                        print(f'Note On: Note={msg.note}')
+                        if msg.note == 60 and count <= 5:                      #Red;60;North
     
-    if LaunchpadPro_Name not in mido.get_input_names():
-        print(f"Device {LaunchpadPro_Name} not found. Please check the device name.")
-        return
-
-    with mido.open_input(LaunchpadPro_Name) as inport, mido.open_output(LaunchpadPro_Name) as outport:
-        print(f"Listening to {LaunchpadPro_Name} for note messages...")
-        try:
-            for msg in inport:
+                            print ('North pressed')
+    
+                            deflect_success()
+    
+                            #Send to attack marker
+    
+                        elif msg.note == 60 and count >= 5:
+                            game_over()
+    
+    
+                        elif msg.note == 65 and count <= 7:                                   #Green;65;South
+    
+                            print ('South')
+                            deflect_success()
+                            #Send to attack marker
+    
+                        elif msg.note == 65 and count >= 7:
+    
+                            game_over()
+    
+                        elif msg.note == 62 and count <= 9:                                  #Blue;62;East
+                            print ("East")
+                            deflect_success()
+                            #send to attack marker
+    
+                        elif msg.note == 62 and count >= 9:
+    
+                            game_over()
+    
+                        elif msg.note == 64 and count <= 13:                                #Yellow;64;West
+                            print ("West")
+                            deflect_success()
+                            #Send to attack marker
+    
+                        elif msg.note == 64 and count >= 13:
+    
+                            game_over()
+            
+            # try:
+                # for msg in inport:
+                    # if msg.type == 'note_on':
+                        # print(f'Note On: Note={msg.note}')
+                        # if msg.note == 60:                   #North ,Red
+                            # print('North pressed')
+                            # Repear_markers.startMk()
+                        # elif msg.note == 65:                 #South ,Green
+                            # print('South pressed')
+                            # Repear_markers.startMk()
+                        # elif msg.note == 62:                 #East ,Yellow
+                            # print('East pressed')
+                            # Repear_markers.startMk()
+                        # elif msg.note == 64:                 #West , Blue
+                            # print('West pressed')
+                            # Repear_markers.startMk()
                 
-                if msg.type == 'note_on':
-                    # Note on messages represent pad presses
-                    print(f'Note On: Note={msg.note}')
-                    if msg.type == 'note_on' and msg.note == 67:
-                        program_change_msg = mido.Message('program_change', program=custom_mode_program)
-                        print (program_change_msg)
-                        outport.send(mido.Message('sysex', data=byte_data)
-                        print("Switching to Custom Mode...")
-                    elif msg.note == 60:#Red;60;North 
-                        print ('North')
-                        markers.marker_1()
-                        play_stop.play_stop()
-                    elif msg.note == 65:#Green;65;South
-                        print ('South')
-                        markers.marker_2()
-                        play_stop.play_stop()
-                    elif msg.note == 62:#Blue;62;East
-                        print ("East")
-                        markers.marker_3()
-                        play_stop.play_stop()
-                    elif msg.note == 64:#Yellow;64;West
-                        print ("West")
-                        markers.marker_4()
-                        play_stop.play_stop()
-                    #print(f'Note On: Note={msg.note} Velocity={msg.velocity}')
-                elif msg.type == 'note_off':
-                    # Note off messages represent pad releases
-                    print(f'Note Off: Note={msg.note}')
-                    #print(f'Note Off: Note={msg.note} Velocity={msg.velocity}')
+                        
+                    elif msg.type == 'note_off':                         # Note off messages represent pad releases
+                        
+                        print(f'Note Off: Note={msg.note}')
+                                
         except KeyboardInterrupt:
-                print("Stopped listening to MIDI messages.")
+            print("Stopped listening to MIDI messages.")
+        
+#if __name__ == "__main__":
+#Midi_LaunchPad_MK3()
 
-
-
-
-if __name__ == "__main__":
-    #Midi_LaunchPad_MK3()
+# while True:
+    # count += 1
+    # print (count)
+    # time.sleep(.7)
