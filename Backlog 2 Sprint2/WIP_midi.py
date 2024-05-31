518e62ef5e2b69e9694f5640c4f2de0536b8b670
Anestesiaa-0512 <jfengtjf@gmail.com>
2024-05-31 15:30:11 +0800

    31/05 edits

--- Backlog 2 Sprint2/WIP_midi.py
+++ Backlog 2 Sprint2/WIP_midi.py
@@ -3,21 +3,28 @@ import reaper_markers
 import play_stop
 import time
 
+global count
 start_time = None  # Initialize start_time as None
 
+North_pressed = 'False'
+South_pressed = 'False'
+East_pressed  = 'False'
+West_pressed  = 'False' 
+
 def game_over():
-    print('Too slow')
+    print('Game over')
     reaper_markers.fail()  # Send back to the start
-    #play_stop.play_stop()  # To pause / not let the game immediately repeat
     exit()  # Stop midi.py
 
 def deflect_success():
+    #show how fast they press button
     global start_time
     current_time = time.time()
     elapsed_time = current_time - start_time
     print(f"{elapsed_time:.2f}")  # Format elapsed time to 2 decimal places
-    reaper_markers.deflect()
-    time.sleep(1.2)
+    
+    reaper_markers.deflect()  #play deflect audio cue
+    time.sleep(1)
 
 def Midi_LaunchPad_MK3():
     global start_time
@@ -33,60 +40,90 @@ def Midi_LaunchPad_MK3():
         start_time = time.time()  # Set start_time when the loop starts
         try:
             while True:
-                # Increment count and sleep for 0.5 seconds
+                # Increment count and sleep for 1 seconds
                 time.sleep(1)
                 count += 1
                 print(count)
                 
                 # Check for new MIDI messages
                 for msg in inport.iter_pending():
+                    global North_pressed
+                    global South_pressed
+                    global East_pressed
+                    global West_pressed
                     if msg.type == 'note_on':  # Note on messages represent pad presses
                         print(f'Note On: Note={msg.note}')
-                        if msg.note == 60 and count <= 15:  # Red;60;North
+                        #North
+                        if msg.note == 60 and 24 >= count >= 21:  # Red;60;North      #1
+                            # count more than/equal to 21 or less than/equal to 24
                             print('North pressed')
                             deflect_success()
                             
-                            time.sleep(.5)
+                            North_pressed = 'True'
                             
-                            reaper_markers.attackMk()
+                            reaper_markers.projectile2()
+                            print('Back to projectile')
                         
-                        elif msg.note == 60 and count >= 16:
+                        elif msg.note == 60 and 25 <= count <= 35:
+                            # count more than/equal to 25 or less than/equal to 37
                             game_over()
-                        elif msg.note == 65 and count <= 20:  # Green;65;South
+                        #North last
+                        
+                        #South
+                        elif msg.note == 65 and 29 <= count <= 33:  # Green;65;South        #4
+                            # count more than/equal to 29 or less than/equal to 36
                             print('South')
-                            ddeflect_success()
-                            
-                            time.sleep(.5)
-                            
-                            reaper_markers.attackMk()
+                            deflect_success()
+                            South_pressed = 'True'
                         
-                        elif msg.note == 65 and count >= 21:
+                        elif msg.note == 65 and (count >= 36 or count in range(21, 34)):
+                        # count more than/equal to 36 or count is between 21 and 30 (inclusive)
                             game_over()
-                        elif msg.note == 62 and count <= 30:  # Blue;62;East
+                        #south last
+                        
+                        #East
+                        elif msg.note == 62 and 27 <= count <= 30:  # Blue;62;East             #3
+                            # count more than/equal to 27 or less than/equal to 32
                             print("East")
                             deflect_success()
+                            East_pressed = 'True'
                             
-                            time.sleep(.5)
-                            
-                            reaper_markers.attackMk()
+                            reaper_markers.projectile4()
                         
-                        elif msg.note == 62 and count >= 31:
+                        elif msg.note == 62 and (34 >= count >= 31 or count in range(21, 26)):
                             game_over()
-                        elif msg.note == 64 and count <= 40:  # Yellow;64;West
+                        #east last
+                        
+                        #west
+                        elif msg.note == 64 and 23 <= count <= 28:  # Yellow;64;West    #2
+                            # count more than/equal to 23 or less than/equal to 28
                             print("West")
                             deflect_success()
+                            West_pressed = 'True'
                             
-                            time.sleep(.5)
-                            
-                            reaper_markers.attackMk()
+                            reaper_markers.projectile3()
                         
-                        elif msg.note == 64 and count >= 41:
+                        elif msg.note == 64 and (34 >= count >= 28 or count in range(21, 27)):
+                            # count more than/equal to 37 or less than/equal to 29
                             game_over()
-                        elif count == 50:
-                            print("Victory")
-                            #send to win marker
+                            
+                            #west last
+                            
                     elif msg.type == 'note_off':  # Note off messages represent pad releases
                         print(f'Note Off: Note={msg.note}')
+                        
+                if count >= 35:
+                    print('Victory')
+                    reaper_markers.victory()
+                    exit()
+                elif count >= 25 and North_pressed == 'False':      #If no buttons are pressed after count 25
+                    game_over()
+                elif count >= 29 and West_pressed == 'False':       #If no buttons are pressed after count 29
+                    game_over()
+                elif count >= 31 and East_pressed == 'False':       #If no buttons are pressed after count 31
+                    game_over()        
+                elif count >= 34 and South_pressed == 'False':      #If no buttons are pressed after count 35
+                    game_over()
         except KeyboardInterrupt:
             print("Stopped listening to MIDI messages.")
 
--- Backlog 2 Sprint2/reaper_markers.py
+++ Backlog 2 Sprint2/reaper_markers.py
@@ -16,34 +16,75 @@ def send_message(receiver_ip, receiver_port, address, message):
 		print("Message not sent")
 
 # FOR INFO: IP address and port of the receiving Raspberry Pi
-PI_A_ADDR = "192.168.254.18"		# Laptop With L-isa's IP Address
+PI_A_ADDR = "192.168.254.18"		# wlan ip
 PORT = 6800
+
+#addr = "/action/40161" # Jump to Marker One
+
 msg = float(1) # Trigger TRUE Value
 
-def marker_1():
+#send_message(PI_A_ADDR, PORT, addr, msg)
+
+def startMk():
 	global PI_A_ADDR
 	global port
 	addr = "/action/40161" # Jump to Marker One
 	global msg
 	send_message(PI_A_ADDR, PORT, addr, msg)
+	print("Start")
 
-def marker_2():
+def projectile1():
 	global PI_A_ADDR
 	global port
 	addr = "/action/40162" # Jump to Marker Two
 	global msg
 	send_message(PI_A_ADDR, PORT, addr, msg)
+	print("Projectile 1")
+	
+def projectile2():
+	global PI_A_ADDR
+	global port
+	addr = "/action/40165" # Jump to Marker Five
+	global msg
+	send_message(PI_A_ADDR, PORT, addr, msg)
+	print("Projectile 2")
+	
+def projectile3():
+	global PI_A_ADDR
+	global port
+	addr = "/action/40166" # Jump to Marker Six
+	global msg
+	send_message(PI_A_ADDR, PORT, addr, msg)
+	print("Projectile 3")
 
-def marker_3():
+def projectile4():
+	global PI_A_ADDR
+	global port
+	addr = "/action/40167" # Jump to Marker Seven
+	global msg
+	send_message(PI_A_ADDR, PORT, addr, msg)
+	print("Projectile 4")
+
+def deflect():
 	global PI_A_ADDR
 	global port
 	addr = "/action/40163" # Jump to Marker Three
 	global msg
 	send_message(PI_A_ADDR, PORT, addr, msg)
+	print("deflect")
 
-def marker_4():
+def fail():
 	global PI_A_ADDR
 	global port
 	addr = "/action/40164" # Jump to Marker Four
 	global msg
 	send_message(PI_A_ADDR, PORT, addr, msg)
+	print("Fail")
+
+def victory():
+	global PI_A_ADDR
+	global port
+	addr = "/action/40168" # Jump to Marker Eight
+	global msg
+	send_message(PI_A_ADDR, PORT, addr, msg)
+	print("Pass")
