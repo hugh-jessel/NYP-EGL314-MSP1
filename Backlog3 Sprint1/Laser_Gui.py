f1aa263be797f16f965b10b0c30e2ad124dfd4f7
kevis <siangkevis@gmail.com>
2024-06-26 16:53:58 +0800

    Update Laser_Gui.py

--- Backlog3 Sprint1/Laser_Gui.py
+++ Backlog3 Sprint1/Laser_Gui.py
@@ -40,8 +40,18 @@ def page1():
     pageclear(pageWindow)
     tk.Label(pageWindow, text="Laser Controls", font="helvetica 12 bold").grid(row=0, column=0, columnspan=3, pady=(0, 10))
 
-    buttonLaserOn = tk.Button(pageWindow, text="Laser On", font=fontM, bg="black", fg="white", command=laser_on).grid(row=1, column=0, padx=(20, 0))
-    buttonLaserOff = tk.Button(pageWindow, text="Laser Off", font=fontM, bg="black", fg="white", command=laser_off).grid(row=1, column=1, padx=(20, 0))
+    buttonLaser1On = tk.Button(pageWindow, text="Laser1 On", font=fontM, bg="black", fg="white", command=laser1_on).grid(row=1, column=0, padx=(20, 0))
+    buttonLaser1Off = tk.Button(pageWindow, text="Laser1 Off", font=fontM, bg="black", fg="white", command=laser1_off).grid(row=1, column=1, padx=(20, 0))
+    buttonLaser2On = tk.Button(pageWindow, text="Laser2 On", font=fontM, bg="black", fg="white", command=laser2_on).grid(row=2, column=0, padx=(20, 0))
+    buttonLaser2Off = tk.Button(pageWindow, text="Laser2 Off", font=fontM, bg="black", fg="white", command=laser2_off).grid(row=2, column=1, padx=(20, 0))
+    buttonLaser3On = tk.Button(pageWindow, text="Laser3 On", font=fontM, bg="black", fg="white", command=laser3_on).grid(row=3, column=0, padx=(20, 0))
+    buttonLaser3Off = tk.Button(pageWindow, text="Laser3 Off", font=fontM, bg="black", fg="white", command=laser3_off).grid(row=3, column=1, padx=(20, 0))
+    buttonLaser4On = tk.Button(pageWindow, text="Laser4 On", font=fontM, bg="black", fg="white", command=laser4_on).grid(row=4, column=0, padx=(20, 0))
+    buttonLaser4Off = tk.Button(pageWindow, text="Laser4 Off", font=fontM, bg="black", fg="white", command=laser4_off).grid(row=4, column=1, padx=(20, 0))
+    buttonLaser5On = tk.Button(pageWindow, text="Laser5 On", font=fontM, bg="black", fg="white", command=laser5_on).grid(row=5, column=0, padx=(20, 0))
+    buttonLaser5Off = tk.Button(pageWindow, text="Laser5 Off", font=fontM, bg="black", fg="white", command=laser5_off).grid(row=5, column=1, padx=(20, 0))
+    buttonLaser6On = tk.Button(pageWindow, text="Laser6 On", font=fontM, bg="black", fg="white", command=laser6_on).grid(row=6, column=0, padx=(20, 0))
+    buttonLaser6Off = tk.Button(pageWindow, text="Laser6 Off", font=fontM, bg="black", fg="white", command=laser6_off).grid(row=6, column=1, padx=(20, 0))
     #buttonLaserSlow = tk.Button(pageWindow, text="Laser Slow", font=fontM, bg="black", fg="white", command=laser_Slow).grid(row=2, column=0, padx=(20, 0))
     #buttonLaserFast = tk.Button(pageWindow, text="Laser Fast", font=fontM, bg="black", fg="white", command=laser_Fast).grid(row=2, column=1, padx=(20, 0))
 
@@ -69,12 +79,52 @@ send_port = 2000
 addr = "/print"
 
 # laser button functions
-def laser_on():
-    msg = ["7, 1, 1", "7, 2, 1", "8, 1, 1", "8, 2, 1", "9, 1, 1", "9, 2, 1"]
+def laser1_on():
+    msg = ["7, 1, 1"]
     send_message(send_addr, send_port, addr, msg)
 
-def laser_off():
-    msg = ["7,1,0", "7,2,0", "8,1,0", "8,2,0", "9,1,0", "9,2,0"]
+def laser1_off():
+    msg = ["7, 1, 0"]
+    send_message(send_addr, send_port, addr, msg)
+
+def laser2_on():
+    msg = ["7, 2, 1"]
+    send_message(send_addr, send_port, addr, msg)
+
+def laser2_off():
+    msg = ["7, 2, 0"]
+    send_message(send_addr, send_port, addr, msg)
+
+def laser3_on():
+    msg = ["8, 1, 1"]
+    send_message(send_addr, send_port, addr, msg)
+
+def laser3_off():
+    msg = ["8, 1, 0"]
+    send_message(send_addr, send_port, addr, msg)
+
+def laser4_on():
+    msg = ["8, 2, 1"]
+    send_message(send_addr, send_port, addr, msg)
+
+def laser4_off():
+    msg = ["8, 2, 0"]
+    send_message(send_addr, send_port, addr, msg)
+
+def laser5_on():
+    msg = ["9, 1, 1"]
+    send_message(send_addr, send_port, addr, msg)
+
+def laser5_off():
+    msg = ["9, 1, 0"]
+    send_message(send_addr, send_port, addr, msg)
+
+def laser6_on():
+    msg = ["9, 2, 1"]
+    send_message(send_addr, send_port, addr, msg)
+
+def laser6_off():
+    msg = ["9, 2, 0"]
     send_message(send_addr, send_port, addr, msg)
 
 #default page on start-up
