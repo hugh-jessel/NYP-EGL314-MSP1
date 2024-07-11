87343ce0c4e20a13c95ac83151a6bf5e4a644e59
kevis <siangkevis@gmail.com>
2024-07-12 01:42:24 +0800

    WORKING CODE
    HARD CODE GONE HARD CODE GONE HARD CODE GONE CAN SLEEP NOW CAN SLEEP NOW CAN SLEEP NOW

--- MVP/Laser_Gui.py
+++ MVP/Laser_Gui.py
@@ -190,63 +190,37 @@ def send_color(receiver_ip, receiver_port, r, g, b):
 def send_brightness(receiver_ip, receiver_port, brightness):
     client = udp_client.SimpleUDPClient(receiver_ip, receiver_port)
     client.send_message("/brightness", [brightness])
+    
+def AllOff():
+    msg = ["1,1,0", "1,2,0", "2,1,0", "2,2,0", "3,1,0", "3,2,0",
+           "4,1,0", "4,2,0", "5,1,0", "5,2,0", "6,1,0", "6,2,0",
+           "7,1,0", "7,2,0", "8,1,0", "8,2,0", "9,1,0", "9,2,0",
+           "10,1,0", "10,2,0", "11,1,0", "11,2,0", "12,1,0", "12,2,0"]
+    
+    y = int(0)
+    while y < len(msg):
+        send_message(send_addr, send_port, addr, msg[y])
+        y += 1
+        if y == len(msg):
+            break
 
 def lasersequence():
-    send_color(PI_B_ADDR, PORT2, 255, 255, 255)  # change to ur choice of colour (255,255,255)
+    send_color(PI_B_ADDR, PORT2, 255, 0, 0)  # change to ur choice of colour (255,255,255)
     send_brightness(PI_B_ADDR, PORT2, 1)   # change brightness (0-1)
-    print("test")
+    print("test")     
 
-    msg = ["7,1,1",
-           "7,2,1",
-           "8,1,1",
-           "8,2,1",
-           "9,1,1",
-           "9,2,1",
-           "6,1,1",
-           "6,2,1",
-           "5,1,1",
-           "5,2,1",
-           "4,1,1",
-           "4,2,1",
-           "4,2,0",
-           "4,1,0",
-           "5,1,0",
-           "5,2,0",
-           "6,1,0",
-           "6,2,0",
-           "7,1,0",
-           "7,2,0",
-           "8,1,0",
-           "8,2,0",
-           "9,1,0",
-           "9,2,0"
-           ]
-    
+    beat_gap = 60/149 # Time interval between beats
     count = 0
-    y = int(0)
-    while y < 10000000000000000000:
-        time.sleep(1)
-        send_message(send_addr,send_port,addr,msg[y])
-        print(msg[count])
+    start_time = time.time()
+    while time.time() - start_time < 30 :
+        time.sleep(beat_gap)
+        if count == (0):
+            AllOff()
+
         count += 1
-        y += 1
-        
 
-def OddChannels():
-    msg = ["1,1,1",
-           "2,1,1",
-           "3,1,1",
-           "4,1,1",
-           "5,1,1",
-           "6,1,1",
-           "7,1,1",
-           "8,1,1",
-           "9,1,1",
-           "10,1,1",
-           "11,1,1",
-           "12,1,1"]
-
-    return msg
+    print(f"Counted {count} beats in {30} seconds.")
+    AllOff()
 
 """ without server
 
